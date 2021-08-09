b4092e6acc1811929d1916e43f854e3ffe0970ea
naravich@enres.co <seagame@seagames-MacBook-Pro.local>
2021-08-09 11:29:25 +0700

    nest g resource cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [CatsModule], // nest g resource cats
   controllers: [AppController],
   providers: [AppService],
 })
